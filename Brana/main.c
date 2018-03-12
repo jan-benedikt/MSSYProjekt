66dbf66848dda5197d7dbd9e50fdcf2da4d33e73
Jan Benedikt <benedikthonza@gmail.com>
2018-03-12 16:35:59 +0100

    Creating new project
    Signed-off-by: Jan Benedikt <benedikthonza@gmail.com>

---
+++ Brana/main.c
@@ -0,0 +1,18 @@
+/*
+ * Brana.c
+ *
+ * Created: 12.3.2018 15:52:23
+ * Author : Benedikt
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
