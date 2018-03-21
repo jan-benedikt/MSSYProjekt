2f17f82d0609ddf37d10eb3b7e2aeb42a4e00ed4
Student <hajda14@gmail.com>
2018-03-21 15:50:25 +0100

    update pro thinkyho

--- Brana/main.c
+++ Brana/main.c
@@ -19,6 +19,8 @@
 #include "halBoard.h"
 #include "halUart.h"
 
+#include "var.h"
+
 /*- Definitions ------------------------------------------------------------*/
 #ifdef NWK_ENABLE_SECURITY
 #define APP_BUFFER_SIZE     (NWK_MAX_PAYLOAD_SIZE - NWK_SECURITY_MIC_SIZE)
@@ -27,47 +29,8 @@
 #endif
 
 /*- Types ------------------------------------------------------------------*/
-typedef enum AppState_t
-{
-	APP_STATE_INITIAL,
-	APP_STATE_IDLE,
-} AppState_t;
 
-typedef struct NWK_DataInd_t
-{
-	uint16_t srcAddr;
-	uint16_t dstAddr;
-	uint8_t srcEndpoint;
-	uint8_t dstEndpoint;
-	uint8_t options;
-	uint8_t *data;
-	uint8_t size;
-	uint8_t lqi;
-	int8_t rssi;
-} NWK_DataInd_t;
 
-typedef struct NWK_DataReq_t
-{
-	// service fields
-	void *next;
-	void *frame;
-	uint8_t state;
-	// request parameters
-	uint16_t dstAddr;
-	uint8_t dstEndpoint;
-	uint8_t srcEndpoint;
-	uint8_t options;
-	#ifdef NWK_ENABLE_MULTICAST
-	uint8_t memberRadius;
-	uint8_t nonMemberRadius;
-	#endif
-	uint8_t *data;
-	uint8_t size;
-	void (*confirm)(struct NWK_DataReq_t *req);
-	// confirmation parameters
-	uint8_t status;
-	uint8_t control;
-} NWK_DataReq_t;
 
 //Registrace endpointu
 NWK_OpenEndpoint(APP_ENDPOINT, funkceObluhy);
