f9c2b952fa43d16c7ba8710e5ddb6443446db143
Jan Benedikt <benedikthonza@gmail.com>
2018-04-11 13:05:30 +0200

    Tady už svět v pořádku není...
    Opět nefunguje memcpy.

--- Brana/lib/dhcp.c
+++ Brana/lib/dhcp.c
@@ -15,13 +15,13 @@
 void dhcp_sendAddress(NWK_DataInd_t *receive){
 	HELLO_PACK_t *test = malloc (sizeof(receive->data));
 	//NWK_DataInd_t newreceive = receive;
-	memcpy(&test, &receive->data, receive->size);
+	memcpy(&test, &receive->data, sizeof(receive->data));
 	
 	static HELLO_ACK_t hello_ack;
 	hello_ack.ADRESA = receive->srcAddr;
 	hello_ack.ID = 0x00;
 	hello_ack.reserved = receive->dstAddr;
-	com_send(0x01, 0x01, hello_ack, 40);
+	com_send(0x01, 0x01, &hello_ack, 40);
 	//com_reply(receive, hello_ack, sizeof(hello_ack));
 }
 
